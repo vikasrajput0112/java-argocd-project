a43bb7d504e500b75e05b620887c053a9df179b3
vikasrajput0112 <devops1@paisalo.in>
2026-01-14 18:33:26 +0530

    Update PageController.java

--- java-app/src/main/java/com/example/web/controller/PageController.java
+++ java-app/src/main/java/com/example/web/controller/PageController.java
@@ -8,7 +8,7 @@ public class PageController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello, this is HomE... page, ONN... date 14 jan 2026";
+        return "Hello, this is HomE... page, evening date 14 jan 2026";
     }
 
     
@@ -21,7 +21,7 @@ public class PageController {
 
     @GetMapping("/contact")
     public String contact() {
-    return "This is the contact page and you can connect at 858803747412";
+    return "This is the contact page and you can connect at 8588037474";
     }
    
 }
