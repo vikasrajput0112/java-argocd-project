37d8904268c352ab953af39bef954ad02913067f
vikasrajput0112 <devops1@paisalo.in>
2026-01-14 15:38:26 +0530

    Update PageController.java

--- java-app/src/main/java/com/example/web/controller/PageController.java
+++ java-app/src/main/java/com/example/web/controller/PageController.java
@@ -8,7 +8,7 @@ public class PageController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello, this is HomE... page, on date 14 jan 2026";
+        return "Hello, this is HomE... page, ONN date 14 jan 2026";
     }
 
     
